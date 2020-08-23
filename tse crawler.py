b3e22318dfa175d5ad3890e0d940d51c2b1a510d
saadati944 <saadati944@gmail.com>
2020-08-23 09:38:08 +0430

    create urls

--- tse crawler.py
+++ tse crawler.py
@@ -4,7 +4,7 @@ def search(name):
     res=requests.get('http://www.tsetmc.com/tsev2/data/search.aspx?skey='+name)
     return res.text
 
-def reform_data(string):
+def reform_search_data(string):
     '''
     the reformed data has three elements:
     0:id
@@ -20,3 +20,10 @@ def reform_data(string):
         except:
             pass
     return reformed
+
+
+def get_history_url(formeditem):
+    return 'http://www.tsetmc.com/Loader.aspx?ParTree=151311&i='+formeditem[0]
+
+def get_table_url(formeditem):
+    return 'http://www.tsetmc.com/tsev2/data/instinfodata.aspx?i='+formeditem[0]+'&c=25+'
