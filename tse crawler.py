198d3adf392be40523560a68653fd56085ce7bc8
saadati944 <saadati944@gmail.com>
2020-08-23 09:38:40 +0430

    extract table from table url

--- tse crawler.py
+++ tse crawler.py
@@ -27,3 +27,14 @@ def get_history_url(formeditem):
 
 def get_table_url(formeditem):
     return 'http://www.tsetmc.com/tsev2/data/instinfodata.aspx?i='+formeditem[0]+'&c=25+'
+
+def extract_table(table_url):
+    res=requests.get(table_url).text
+    table=[]
+    lines=res.split(';')
+    rows=lines[2][0:-1].split(',')    
+    for j in range(len(rows)):
+        row=rows[j].split('@')
+        for i in range(len(row)-1,-1,-1):
+            table.append(row[i])
+    return table
