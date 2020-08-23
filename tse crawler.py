80cdb399436be15c95b6a2a08b6040d803dceedd
saadati944 <saadati944@gmail.com>
2020-08-23 09:36:14 +0430

    reform returned search data

--- tse crawler.py
+++ tse crawler.py
@@ -3,3 +3,20 @@ import requests
 def search(name):
     res=requests.get('http://www.tsetmc.com/tsev2/data/search.aspx?skey='+name)
     return res.text
+
+def reform_data(string):
+    '''
+    the reformed data has three elements:
+    0:id
+    1:name
+    2:description
+    '''
+    reformed=[]
+    lines=string.split(';')
+    for line in lines :
+        try:
+            data=line.split(',')
+            reformed.append([data[2],data[0],data[1]])
+        except:
+            pass
+    return reformed
